e5e289fe72d0be3526140ddbb4ae9456a4c3b737
Kirill <kirilltrezubov@yandex.ru>
2020-06-09 20:52:08 +0300

    finish spring lesson 17

---
+++ src/main/java/ru/kirilltrezubov/springcourse/controllers/SecondController.java
@@ -0,0 +1,9 @@
+package ru.kirilltrezubov.springcourse.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecondController {
+    @GetMapping("/exit")
+    public String exit() {return "second/exit";}
+}
